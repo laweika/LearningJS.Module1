c4cf654616f7d5701fa546b63ea855ae0df1c8a5
Victoria Rakhimova <rahimova96@mail.ru>
2022-01-14 00:06:10 +0700

    Lesson 2 is done

---
+++ Lesson 2/script.js
@@ -0,0 +1,18 @@
+'use strict'
+
+{
+	const productName = "Shampoo";
+	const productCategory = "Hair products";
+	const productNumber = 100;
+	const productPrice = 1999;
+
+	console.log(productName);
+}
+
+
+{
+	const productName = "Mazda 3";
+	const productCategory = "Cars";
+	const productNumber = 1500000;
+	const productPrice = 10;
+}
