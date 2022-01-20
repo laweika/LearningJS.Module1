c69bccee61887b8352d74ea3f335ea1218834e7f
Victoria Rakhimova <rahimova96@mail.ru>
2022-01-21 02:24:37 +0700

    Lesson 7 is done(+corrected lesson 6)

---
+++ Lesson 7/Task01.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/*
+Напишите функцию filter()
+функция принимает в параметрах 2 массива
+Первый массив — список всех студентов, второй — список студентов не сдавших экзамен.
+Результат функции: массив из всех студентов, которые сдали экзамен.*/
+
+const allStudentsList = ['Петров', 'Иванов', 'Васечкин', 'Харламов', 'Зверьков', 'Попов'];
+const failedExamStudentsList = ['Иванов', 'Васечкин'];
+
+const filter = (allStudentsList, failedExamStudentsList) => {
+
+	const passedExamStudentsList = allStudentsList.splice(failedExamStudentsList.length);
+
+	return passedExamStudentsList;
+}
+
+console.log(filter(allStudentsList, failedExamStudentsList));
---
+++ Lesson 7/Task02.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/*
+Напишите функцию getAverageValue
+для получения среднеарифметического значения с округлением в меньшую сторону до целого числа
+С помощью этой функции получите средний чек за день, в массиве данные всех чеков за день:
+const allСashbox = [4500, 3210, 650, 1250, 7830, 990, 13900, 370];
+*/
+
+const allСashbox = [4500, 3210, 650, 1250, 7830, 990, 13900, 370];
+
+const getAverageValue = () => {
+	let allСashboxTotalSum = 0;
+
+	for (let i = 0; i < allСashbox.length; i++) {
+
+		allСashboxTotalSum = allСashbox[i] + allСashboxTotalSum;
+		console.log(allСashboxTotalSum);
+	}
+
+	const averageValue = Math.floor(allСashboxTotalSum / allСashbox.length);
+	return averageValue;
+};
+
+console.log(getAverageValue(allСashbox));
