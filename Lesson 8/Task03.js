88405201b356d3aa3454e7525785a9428c3e869c
Victoria Rakhimova <rahimova96@mail.ru>
2022-02-08 22:01:06 +0700

    Lesson 8. Task 3 is in process

--- Lesson 8/Task03.js
+++ Lesson 8/Task03.js
@@ -11,46 +11,33 @@
 
 const getRandomArr3 = (arrLength, n, m, str) => {
 	let arr = [];
-	str === '';
-	arrLength === arr.length;
+	let filteredArr = [];
 
 	const max = Math.max(n, m);
 	const min = Math.min(n, m);
 
 	for (let i = 0; i < arrLength; i++) {
-		let num = arr[i];
-		num = Math.floor(Math.random() * (max - min + 1)) + min;
-
-		if (str === 'even') {
-			while (num % 2 !== 0) {
-				num = + 1;
-				continue;
-			}
-		} else if (str === 'odd') {
-			while (num % 2 === 0) {
-				num = + 1;
-				continue;
-			}
-		}
+		arr[i] = Math.floor(Math.random() * (max - min + 1)) + min;
 	}
 
 	console.log(arr);
 
+
+	if (str === 'even') {
+		filteredArr = arr.filter((num) => num % 2 === 0);
+	}
+
+	if (str === 'odd') {
+		filteredArr = arr.filter((num) => num % 2 !== 0)
+	}
+
+	while (filteredArr.length < arrLength) {
+		filteredArr.push(??);
+		continue;
+	}
+
+	return filteredArr;
 }
 
 
-getRandomArr3(10, 20, 40, 'even');
-
-
-
-
-
-
-/*
-if (num % 2 !== 0) {
-	num = + 1;
-	return str === 'odd';
-} else {
-	return str === 'even';
-};
-*/
+console.log(getRandomArr3(10, 20, 40, 'even'));
